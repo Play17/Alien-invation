1c81b861770b7ca0bdc64b10fbd356ec2b0a1d03
Play17 <50292177+Play17@users.noreply.github.com>
2020-03-05 20:56:08 +0800

    Update settings.py

--- alien_invation/settings.py
+++ alien_invation/settings.py
@@ -3,16 +3,21 @@ class Settings():
     def __init__(self):
         """初始化游戏的设置"""
         #屏幕设置
-        self.screen_width = 600
-        self.screen_height = 400
-        self.bg_color = (255,255,255)
+        self.screen_width = 1200         #窗口宽
+        self.screen_height = 600        #窗口高
+        self.bg_color = (255,255,255)   #窗口背景色，白色
         
         #飞船移动速度
-        self.ship_speed_factor = 0.2
+        self.ship_speed_factor = 1    
         
         #子弹设置
-        self.bullet_speed_factor = 0.1
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60,60,60
-        self.bullets_allowed = 3
+        self.bullet_speed_factor = 1    #子弹速度
+        self.bullet_width = 3           #子弹宽
+        self.bullet_height = 15         #子弹高
+        self.bullet_color = 60,60,60    #子弹颜色，黑色
+        self.bullets_allowed = 5        #子弹数量
+        
+        #外星人设置 
+        self.alien_speed_factor = 0.5   #外星人移动速度
+        self.fleet_drop_speed = 10      #外星人下落速度
+        self.fleet_direction = 1        #1代表往右移动，-1代表往左移动
