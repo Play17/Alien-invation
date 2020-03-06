b5f7b84cf3893478c3d627c99187bd5d02997ee9
Play17 <50292177+Play17@users.noreply.github.com>
2020-03-06 15:20:52 +0800

    Add files via upload

---
+++ 抓球/settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = 255,255,255
+
+        self.ball_drop_speed = 1
+        self.receiver_speed_factor = 3
+
+        self.count = 0
