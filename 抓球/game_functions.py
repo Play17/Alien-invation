f7eb2515357dbf0f5080ca7b54a798797ee1bb9f
Play17 <50292177+Play17@users.noreply.github.com>
2020-03-07 10:26:56 +0800

    Update game_functions.py

--- 抓球/game_functions.py
+++ 抓球/game_functions.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from ball import Ball
 from random import randint
+from time import sleep
 
 def check_events(receiver):
     for event in pygame.event.get():
@@ -35,13 +36,24 @@ def update_screen(ai_settings, screen, receiver,ball):
 def update_balls(ai_settings,screen,ball,receiver):
         ball.update()
         
+        #如果球与接球者发生碰撞，改变球的位置，输出改变了几次，也就代表你接住几次球.
         if pygame.sprite.collide_rect(ball, receiver):
             change_ball_rect(ball,ai_settings,screen)
             print(ai_settings.count)
         
+        #如果没有接到球,球的个数减1，并暂停0.5秒，再改变球的位置
         if ball.rect.bottom >= ai_settings.screen_height:
-            pygame.quit()
-            sys.exit()
+            if ai_settings.balls > 1:
+                ai_settings.balls -= 1 
+                sleep(0.5)
+                change_ball_rect(ball,ai_settings,screen)
+            #如果没有球了，就不再改变球的位置，并打印game over
+            else:
+                ball.game_active == False
+                print('game over')
+                
+                #pygame.quit()
+                #sys.exit()
             #ball.rect.x = randint(1,ai_settings.screen_width)
             #ball.rect.y = 0
             
