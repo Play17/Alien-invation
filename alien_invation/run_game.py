3abfbce5b6842403652c8d24153490feadab80e7
Play17 <50292177+Play17@users.noreply.github.com>
2020-03-12 01:17:09 +0800

    Update run_game.py

--- alien_invation/run_game.py
+++ alien_invation/run_game.py
@@ -4,6 +4,8 @@ from ship import Ship       #导入ship.py的Ship类
 import game_functions as gf  #导入game_functions.py并设标签gf
 from pygame.sprite import Group     
 from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 """
     
    按 F5 运行这个程序 ，你会启动Alicen Invasion游戏  按左右控制飞船移动
@@ -21,33 +23,34 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     #设置窗口名为Alien Invasion
     pygame.display.set_caption("Alien Invasion")  
+    #创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
+    #创建统计信息
     stats = GameStats(ai_settings)
-
+    sb = Scoreboard(ai_settings, screen, stats)
     #创建一艘飞船,一个用于存储子弹的编组和一个外星人编组
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    
-    
     #创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
     
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
         
         if stats.game_active:
             #更新飞船位置
             ship.update()
             #删除已消失的子弹
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             #print(len(bullets))
             #更新外星人位置
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets)
         #更新屏幕上的图像，并切换到新屏幕
         
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
